b0f7a74879eb6b5f2ab28c6cff7bf8496ddf0621
ankit prajapati <91253833+Ankit1433@users.noreply.github.com>
2022-09-15 19:53:10 +0530

    Update hello.java

--- src/nimti/hello.java
+++ src/nimti/hello.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class hello {
+ublic class twopointers {
+    static boolean func(int arr[],int x){
+        int i=0,j=arr.length-1;
+        while(i<j){
+            if(arr[i]+arr[j]==x){
+                return true;
+            } else if (arr[i]+arr[j]>x) {
+                j=j-1;
+            }
+            else {
+                i=i+1;
+            }
+
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        int arr[]={2,5,8,16,24,30};
+        int x=40;
+        System.out.println(func(arr,x));
+
+    }
+
 }
