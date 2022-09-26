61dee6ff1e028480b8b867f1290811f12865a2c1
ankit prajapati <91253833+Ankit1433@users.noreply.github.com>
2022-09-26 20:34:54 +0530

    lomuto partion in quick sorting

--- src/nimti/sorting/lomutopartionqs.java
+++ src/nimti/sorting/lomutopartionqs.java
@@ -1,5 +1,28 @@
 package nimti.sorting;
 
 public class lomutopartionqs {
-    
+    static int func(int arr[],int low,int high){
+      int i=low-1,pivot=arr[high];;
+      for(int j=low;j<=high-1;j++){
+          if(arr[j]<pivot){
+              i++;
+              int temp=arr[i];
+              arr[i]=arr[j];
+              arr[j]=temp;
+          }
+      }
+      int temp2=arr[high];
+      arr[high]=arr[i+1];
+      arr[i+1]=temp2;
+        return i+1;
+    }
+
+    public static void main(String[] args) {
+    int arr[]={10,80,30,90,40,50,70};
+    int low=0;
+    int high=arr.length-1;
+        System.out.println(func(arr,low,high));
+        for(int i:arr)
+            System.out.print(i+" ");
+    }
 }
